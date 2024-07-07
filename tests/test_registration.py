44fe0889e98cc3d0474ba244205077570aa45748
Dmitriy Karpov <dvkkdv@mail.ru>
2024-07-07 11:53:28 +0700

    corrections

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,4 @@
 import time
-import random
 
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -10,11 +9,6 @@ from constants import Constants
 
 class TestStellarBurgersRegistration:
 
-    @staticmethod
-    def login_generation():
-        login = f"dmitriy_karpov_07_{random.randint(1000, 9999)}@yandex.ru"
-
-        return login
 
     def test_registration_positive_input_successful(self, driver):
 
@@ -24,16 +18,15 @@ class TestStellarBurgersRegistration:
         driver.find_element(*Locators.REGISTER_BUTTON_LINK).click()
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(Locators.INPUT_ELEMENT))
-        login = self.login_generation
         driver.find_element(*Locators.NAME_NAME).send_keys(Constants.NAME)
-        driver.find_element(*Locators.NAME_EMAIL).send_keys(login)
+        driver.find_element(*Locators.NAME_EMAIL).send_keys(Constants.LOGIN_GENERATION)
         driver.find_element(*Locators.PASSWORD).send_keys(Constants.PASSWORD)
         WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable(Locators.REGISTER_BUTTON))
         driver.find_element(*Locators.REGISTER_BUTTON).click()
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(Locators.AUTH_LOGIN_ELEMENT))
         time.sleep(1)
-        driver.find_element(*Locators.NAME_EMAIL).send_keys(login)
+        driver.find_element(*Locators.NAME_EMAIL).send_keys(Constants.LOGIN_GENERATION)
         driver.find_element(*Locators.PASSWORD).send_keys(Constants.PASSWORD)
         WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable(Locators.LOGIN_BUTTON))
         driver.find_element(*Locators.LOGIN_BUTTON).click()
@@ -47,7 +40,7 @@ class TestStellarBurgersRegistration:
         user_password = driver.find_element(*Locators.FIELD_USER_PASSWORD).get_attribute("value")
 
         assert user_name == Constants.NAME
-        assert user_email == login
+        assert user_email == Constants.LOGIN_GENERATION
         assert len(user_password) != 0
 
         driver.find_element(*Locators.LOGOUT_BUTTON).click()
